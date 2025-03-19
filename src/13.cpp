163fcb1461e23b805b38777d10612777c49b3b1a
vividoumbouya245 <vividoumbouya245@gmail.com>
2025-03-19 16:44:48 +0000

    Add 13.cpp

---
+++ src/13.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name = "John";
+    int age = 30;
+
+    cout << "Hello, my name is " << name << " and I am " << age << " years old." << endl;
+    return 0;
+}
