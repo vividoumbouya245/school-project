a77d9ec4ecd55b2db9d006b991c4ac115c90a59f
vividoumbouya245 <vividoumbouya245@gmail.com>
2025-03-30 10:31:18 +0000

    Add 19.cpp

---
+++ src/19.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        cout << "The number is positive." << endl;
+    } else {
+        cout << "The number is not positive." << endl;
+    }
+    return 0;
+}
