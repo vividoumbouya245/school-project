f29524efd248ad68c8b4c62d5ee92511306ea2fc
vividoumbouya245 <vividoumbouya245@gmail.com>
2025-04-06 00:35:31 +0000

    Add 25.cpp

---
+++ src/25.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 10;
+    
+    if (a > b) {
+        cout << "The value of a is greater than b." << endl;
+    } else {
+        cout << "The value of a is not greater than b." << endl;
+    }
+    
+    return 0;
+}
