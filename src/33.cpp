9dd4359e8e27c3bd665cedc5c365e0d6cf5d5b0f
vividoumbouya245 <vividoumbouya245@gmail.com>
2025-04-27 01:58:14 +0000

    Add 33.cpp

---
+++ src/33.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 10;
+    if (x > y) {
+        cout << "x is greater than y" << endl;
+    } else {
+        cout << "y is greater than x" << endl;
+    }
+}
