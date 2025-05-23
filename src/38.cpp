ee4cec3013327ccf09a64ec986e83f29d605d100
vividoumbouya245 <vividoumbouya245@gmail.com>
2025-05-23 07:59:30 +0000

    Add 38.cpp

---
+++ src/38.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    if(x > 10) {
+        cout << "x is greater than 10" << endl;
+    } else {
+        cout << "x is not greater than 10" << endl;
+    }
+}
